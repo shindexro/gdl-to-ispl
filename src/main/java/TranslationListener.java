747ef7e2ea28070d9e12d6cd883ebff02850e743
shindexro <shindexro@gmail.com>
2021-06-24 19:14:59 +0800

    Detect role rules

--- src/main/java/TranslationListener.java
+++ src/main/java/TranslationListener.java
@@ -1,7 +1,27 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class TranslationListener extends GDLBaseListener {
     private GDLParser parser;
+    private Set<String> roles;
 
     public TranslationListener(GDLParser parser) {
         this.parser = parser;
+        this.roles = new HashSet<>();
+    }
+
+    @Override
+    public void enterRelationLiteral(GDLParser.RelationLiteralContext ctx) {
+        super.enterRelationLiteral(ctx);
+        String predicate = ctx.CONST().getText();
+
+        if (predicate.equals("role")) {
+            GDLParser.TermContext term = ctx.term(0);
+            while (term.term() != null)
+                term = term.term();
+            String role = term.CONST().getText();
+            roles.add(role);
+        }
     }
 }
